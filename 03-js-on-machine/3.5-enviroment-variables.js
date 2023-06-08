3bb40702dab1d9e5a1252df610ad8749ae45dae1
sdahal1 <raabdahl@gmail.com>
2023-06-08 11:58:07 -0700

    w1d4pm

--- 03-js-on-machine/3.5-enviroment-variables.js
+++ 03-js-on-machine/3.5-enviroment-variables.js
@@ -1,5 +1,5 @@
-console.log(process.env) //environment variables in an object
+console.log(process.env) //environment variables in an object.
 
 
 
-console.log(process.argv) //script arguments in an array
+console.log(process.argv) //script arguments in an array.
