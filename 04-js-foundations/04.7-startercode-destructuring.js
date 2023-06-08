957dc92300899f0949b52a3d46219c9317d00ef4
sdahal1 <raabdahl@gmail.com>
2023-06-08 08:17:36 -0700

    w1d4am

--- 04-js-foundations/04.7-startercode-destructuring.js
+++ 04-js-foundations/04.7-startercode-destructuring.js
@@ -17,6 +17,7 @@ function printAuthorAndTitle(product) {
     return `${product.title} by ${product.author.firstName} ${product.author.surname}`;
 }
 
+// console.log(printAuthorAndTitle(product))
 
 
 // - EXAMPLE WITHOUT DESTRUCTURING BUT USING VARIABLES INSTEAD
@@ -30,41 +31,91 @@ function printAuthorAndTitle2(product) {
 // - EXAMPLE WITH DESTRUCTURING
 
 function printAuthorAndTitle3(product) {
-
+  const {title, author, priceInCents, tax} = product;
+  return `${title} by ${author.firstName} ${author.surname}`;
 }
 
 
 
-// - WHAT HAPPENS IF THERE IS MISSING KEYS?
+
+// - WHAT HAPPENS IF THERE IS MISSING KEYS? - variable will be undefined
 
 
 
 // - DESTRUCTURING A NESTED OBJECT WITH NESTED KEYS
+function printAuthorAndTitle4(product) {
+  const {title, author:{firstName,surname}} = product;
+  
+  return `${title} by ${firstName} ${surname}`;
+}
 
+// console.log(printAuthorAndTitle4(product))
 
 
 // - RENAMING A DESTRUCTURED VARIABLE TO BE DIFFERENT FROM THE KEY NAME
+function printAuthorAndTitle5(product) {
+  const {title:bookTitle, author:{firstName:nombre,surname}} = product;
+  
+  return `${bookTitle} by ${nombre} ${nombre}`;
+}
 
-
+// console.log(printAuthorAndTitle5(product))
 
 
 // - DESTRUCTURING ARRAYS
 
-// const genres = [
-//   "Fantasy",
-//   "Fiction",
-//   "Nonfiction",
-//   "Science Fiction",
-//   "Young Adult",
-// ];
+const genres = [
+  "Fantasy",
+  "Fiction",
+  "Nonfiction",
+  "Science Fiction",
+  "Young Adult",
+];
 
+// console.log(`My fav genres are ${genres[4]}, ${genres[2]} and ${genres[0]}`)
+
+const [,,,x,y] = genres;
+// console.log(x,y)
 
 
 // - THE REST OPERATOR ON OBJECTS
+const {title, ...everythingElse} = product;
 
+// console.log(title)
+// console.log(everythingElse)
 
 // - THE REST OPERATOR ON ARRAYS
+const [,a, ...otherElements] = genres;
 
-
+// console.log(otherElements)
 
 // - DESTRUCTURING PARAMETERS
+
+
+function printAuthorAndTitle6({title, author:{firstName,surname}}) {
+  // const {title, author:{firstName,surname}} = product;
+  return `${title} by ${firstName} ${surname}`;
+}
+
+
+// console.log(printAuthorAndTitle6(product))
+
+
+const advancedProduct = {
+  name: "Geometric Print Swim Shorts",
+  colors: {
+    pink: {
+      priceInCents: 1499,
+      availableSizes: ["XS", "S", "M", "XL"],
+    },
+    blue: {
+      priceInCents: 999,
+      availableSizes: ["M", "L", "XL"],
+    },
+  },
+};
+
+
+const {name, colors:{pink:{availableSizes:[,,medium]}} } = advancedProduct
+
+console.log(name, medium)
---
+++ 04-js-foundations/04.8-startercode-object-shorthand-and-spread.js
@@ -0,0 +1,47 @@
+/* 
+Object shorthand: 
+JavaScript syntax that supports the creation of objects using values stored in variables, where the variable name becomes the key and the stored value becomes the value in the key-value pair
+
+*/
+
+
+const title = "The Presence Process";
+const author = "Michael Brown";
+// CREATE AN OBJECT USING THOSE VARIABLES AS KEYS AND THEIR VALUES AS VALUES- NAME IT "PRODUCT"
+// const product = {
+//   title: title,
+//   author: author
+// }
+
+//OBJECT SHORTHAND- EXAMPLE:
+const product = {
+  title,
+  author
+}
+
+// console.log(product)
+
+
+
+
+
+//THE SPREAD OPERATOR-> USE IT TO MAKE A COPY OF AN OBJECT OR ARRAY
+
+//THE SPREAD OPERATOR-> USE IT TO COMBINE TWO ARRAYS INTO ONE
+const ownedBooks = ["Infernal Devices", "Foundation", "Food"];
+const wishlist = ["Good Omens", "Guards! Guards!"];
+
+
+const combined = [...ownedBooks, ...wishlist]
+// console.log(combined)
+
+//THE SPREAD OPERATOR-> USE IT TO COMBINE TWO OBJECTS INTO ONE
+const salesTax = { state: "Washington", tax: 0.065, title: "tax title" };
+//combine salesTax with the product variable from above
+
+
+const combinedObjects = {...product, ...salesTax, tax: 0.5}
+
+console.log(combinedObjects)
+
+//THE SPREAD OPERATOR-> USE IT TO COMBINE TWO OBJECTS INTO ONE AND OVERWRITE ONE OF THE EXISTING KEYS 
