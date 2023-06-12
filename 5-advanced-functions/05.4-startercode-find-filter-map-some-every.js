9d52b110647e251122f1e10a1e2af0f85f55b774
sdahal1 <raabdahl@gmail.com>
2023-06-12 12:05:34 -0700

    w2d1pm

--- 5-advanced-functions/05.4-startercode-find-filter-map-some-every.js
+++ 5-advanced-functions/05.4-startercode-find-filter-map-some-every.js
@@ -80,23 +80,78 @@ const employees = [
       state: "New York",
     },
     languages: ["Javascript", "Java", "Python"],
-  },
+  }
 ];
 
 //find an employee who is named "Larry David"
-function findEmployeeByName(employees, name) {}
+function findEmployeeByName(employees=[], name) {
+  const result = employees.find((element,index)=>{
+    return element.name === name
+  })
+  if(result === undefined) return null;
+  return result;
+
+  // return employees.find((element, index)=> element.name === name)
+}
+
+//one liner way
+// const findEmployeeByName = (employees, name) =>  employees.find((element, index)=> element.name === name)
+
+// console.log(findEmployeeByName(employees, "Scooby Doo"))
 
 //get all the employees who are making over a given amount
-function findHighEarners(employees, amount) {}
+function findHighEarners(employees, amount) {
+  const result = employees.filter((element)=>{
+    //return a condition that evaluates to a boolean
+    return element.salary > amount
+  })
+  return result;
+}
 
-//give back a new array containing only the company names and city for each employee in the given list
-function findCompanyNamesAndCity(employees) {}
+// console.log(findHighEarners(employees, 100000))
+
+// let nums = [2,4,5,6,7]
+// let result = nums.map((element)=>{
+//   //return the transformation
+//   if(element < 5){
+//     return `hello, the lil' number is ${element}`
+//   }else{
+//     return `woah big time number of ${element}`
+//   }
+// })
+
+// console.log(result)
+
+
+//give back a new array containing only the company names and city for each employee in the given list formatted as "companyNameHere - cityHere"
+function findCompanyNamesAndCity(employees) {
+  const result = employees.map((element)=>{
+    return `${element.company.name} - ${element.company.city}`
+  })
+  return result;
+}
+
+// console.log(findCompanyNamesAndCity(employees))
 
 //use .some() to check if any employees are from a company with the name "Comedy Central"
-function doesCompanyHaveEmployee(employees, companyName) {}
+function doesCompanyHaveEmployee(employees, companyName) {
+  const result = employees.some((element)=>{
+    return element.company.name === companyName
+  })
+  return result;
+}
 
-//use .every() to indicate whether every employee is making over a certain salary
-function areAllEmployeesGettingPaidGivenAmount(employees, amount) {}
+// console.log(doesCompanyHaveEmployee(employees, "Comedy Central"))
+
+//use .every() to indicate whether every employee is making at least a certain salary
+function areAllEmployeesGettingPaidGivenAmount(employees, amount) {
+  const result = employees.every((element)=>{
+    return element.salary >= amount
+  })
+  return result
+}
+
+// console.log(areAllEmployeesGettingPaidGivenAmount(employees, 100000))
 
 
 
@@ -105,9 +160,42 @@ function areAllEmployeesGettingPaidGivenAmount(employees, amount) {}
 
 /* ADVANCED: HOW TO COMBINE THESE METHODS FOR MORE COMPLEX PROBLEMS */
 
-/* USE FILTER() AND MAP() TO GIVE BACK A NEW ARRAY CONTAINING ONLY THE COMPANY NAMES AND CITY FOR EACH EMPLOYEE IN THE GIVEN LIST WHO HAVE A SALARY GREATER THAN OR EQUAL TO A GIVEN AMOUNT
+/* USE __filter__() AND _map___() TO GIVE BACK A NEW ARRAY CONTAINING ONLY THE COMPANY NAMES AND CITY FOR EACH EMPLOYEE IN THE GIVEN LIST WHO HAVE A SALARY GREATER THAN OR EQUAL TO A GIVEN AMOUNT
 
-HINT: USE .FILTER() TO GET BACK ONLY THE EMPLOYEES WHO MAKE A GIVEN AMOUNT OR MORE, THEN USE .MAP TO TRANSFORM THAT DATA TO A NEW ARRAY CONTAINING THE COMPANY NAMES AND CITY OF THOSE EMPLOYEES
+
+HINT: USE ._filter__() TO GET BACK ONLY THE EMPLOYEES WHO MAKE A GIVEN AMOUNT OR MORE, THEN USE ._map__ TO TRANSFORM THAT DATA TO A NEW ARRAY CONTAINING THE COMPANY NAMES AND CITY OF THOSE EMPLOYEES
 */
 
-/* USE FILTER() AND SOME() METHOD TO FIND IF ANY EMPLOYEE FROM A GIVEN STATE HAS A SALARY OF OVER A GIVEN AMOUNT */
+function highPayingCompanyNames(employees, amount){
+  //first get the filtered result of employes making at least given amount
+  const highEarners = employees.filter((element)=>{
+    return element.salary >= amount
+  })
+  //then transform the filtered dataset to only get the names and city of the companies
+  const result = highEarners.map((element)=>{
+    const {company:{name,city}} = element;
+    return `${name} - ${city}`
+  })
+
+  return result;
+}
+
+
+const highPayingCompanyNamesAdvanced = (employees, amount)=>employees.filter((element)=> element.salary >= amount).map(({company:{name,city}})=> `${name} - ${city}`)
+
+
+// console.log(highPayingCompanyNamesAdvanced(employees, 100000))
+
+
+/* USE ____() AND ____() METHOD TO FIND IF ANY EMPLOYEE FROM A GIVEN STATE HAS A SALARY OF OVER A GIVEN AMOUNT */
+
+function isAnybodyFromGivenStateMakingOverGivenAmount(employees, stateName, amount){
+  const result = employees.some((element)=>{
+    return element.company.state === stateName && element.salary > amount
+  })
+
+  return result
+}
+
+
+console.log(isAnybodyFromGivenStateMakingOverGivenAmount(employees, "New York", 100000))
