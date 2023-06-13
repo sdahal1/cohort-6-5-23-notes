429371587ea54efa07a548d46c2641bf3e8aab42
sdahal1 <raabdahl@gmail.com>
2023-06-13 12:04:11 -0700

    w2d2pm

--- 5-advanced-functions/05.6-startercode-sort.js
+++ 5-advanced-functions/05.6-startercode-sort.js
@@ -4,7 +4,7 @@
   -INPUT: CALLBACK FUNCTION
   -RETURNS: SORTED ARRAY
   -CALLBACK FUNCTION DETAILS:
-    -CB PARAMETERS: TWO ELEMENTS TO BE COMPARED
+    -CB PARAMETERS: TWO ELEMENTS TO BE COMPARED (elementA, elementB)
     -CB RULE: RETURN A NUMBER (POSITIVE, NEGATIVE, 0) - the callback function has to return a value indicating the relationship between the two elements.
       -NEGATIVE NUMBER: If the returned value is negative, the first element will be sorted before the second element. 
       -POSITIVE NUMBER: If the returned value is positive, the second element will be sorted before the first element. 
@@ -18,16 +18,23 @@
 /* EXAMPLE 1: SORTING NUMBERS- GIVEN AN ARRAY OF NUMBERS */
 const numbers = [23,4,12,77,-3,-5,1]
 
+
 //ASCENDING (LEAST TO GREATEST)
 function sortAscending(nums){
-
+  nums.sort((elementA, elementB)=>{
+    return elementA - elementB
+  })
+  return nums
 }
 
 // console.log(sortAscending(numbers))
 
 //DESCENDING
 function sortDescending(nums){
-
+  nums.sort((elementA, elementB)=>{
+    return elementB - elementA
+  })
+  return nums
 }
 
 // console.log(sortDescending(numbers))
@@ -41,11 +48,46 @@ const parks = [
     { name: "Indiana Dunes", rating: 4.1 },
   ];
 
+function sortParksDescending(parks){
+  parks.sort((elementA,elementB)=>{
+    return elementB.rating - elementA.rating
+  })
+  return parks
+}
+// console.log(sortParksDescending(parks))
 
 
 /* EXAMPLE 3: SORTING STRINGS */
+function sortParksAlphabetically(parks){
+  // parks.sort((elementA, elementB)=>{
+  //   //can only return number
+  //   if(elementA.name.toLowerCase() < elementB.name.toLowerCase()){
+  //     return -1
+  //   }else{
+  //     return 1
+  //   }
+  // })
 
+  parks.sort((elementA, elementB)=>{
+    //can only return number
+    return elementA.name.toLowerCase() < elementB.name.toLowerCase() ? -1 : 1
+  })
+  return parks
+}
+
+// console.log(sortParksAlphabetically(parks))
 
 /* EXAMPLE 4: SORT STRINGS BY LENGTH */
 
+function sortParksByNameLength(parks){
+  parks.sort((elementA, elementB)=>{
+    //can only return number
+    return elementA.name.length - elementB.name.length
+  })
 
+  
+  return parks
+}
+
+
+console.log(sortParksByNameLength(parks))
--- 5-advanced-functions/05.7-startercode-assessment-studyguide.js
+++ 5-advanced-functions/05.7-startercode-assessment-studyguide.js
@@ -56,7 +56,19 @@ const students = {
 /*
 1. GET BOOKS BY PUBLISHER NAME
 */
-function getBooksByPublisher(books, publisherName) {}
+function getBooksByPublisher(books, publisherName) {
+  const result = books.filter((element)=>{
+    //return something (condition) that evaluates to a boolean
+    // if(element.publisher.name === publisherName){
+    //   return true;
+    // }else{
+    //   return false;
+    // }
+    return element.publisher.name === publisherName
+  })
+
+  return result;
+}
 
 // console.log(getBooksByPublisher(books, "Simon & Schuster"));
 
@@ -67,11 +79,35 @@ WRITE A FUNCTION THAT TAKES AN ARRAY OF BOOKS, OBJECT CONTAINING ALL STUDENTS, A
 IT WILL RETURN AN ARRAY OF BOOK OBJECTS THAT REPRESENT THE BOOKS THAT THE GIVEN USER HAS FINISHED.
 */
 
-function getFinishedBooksFromStudent(){
+function getFinishedBooksFromStudent(books=[],students={},studentName=""){
+  //looked at students object first at the key representing the studentName
+  const finished = students[studentName].finished //[1,2]
+  const result = []
+  //look at the books array and forEach element, check if the id is inside of finished. If it is, push it to our result
+  books.forEach((bookObj)=>{
+    if(finished.includes(bookObj.id)){
+      result.push(bookObj)
+    }
+  })
 
+  return result;
 }
 
-// console.log(getFinishedBooksFromStudent(books, students, "Spongebob"))
+function getFinishedBooksFromStudentAdvanced(books=[],students={},studentName=""){
+  //looked at students object first at the key representing the studentName
+  const finished = students[studentName].finished //[1,2]
+  //look at the books array and forEach element, check if the id is inside of finished. If it is, push it to our result
+  const result = books.filter((bookObj)=>{
+    return finished.includes(bookObj.id)
+  })
+
+  return result;
+}
+
+// console.log(getFinishedBooksFromStudent(books, students, "Squidward"))
+// console.log(getFinishedBooksFromStudentAdvanced(books, students, "Spongebob"))
+
+
 
 /* 
 3. FIND IF A GIVEN STUDENT HAS READ ALL THE BOOKS FROM A GIVEN PUBLISHER. RETURN TRUE OR FALSE
@@ -81,11 +117,36 @@ FUNCTION INPUTS: LIST OF BOOKS, OBJECT CONTAINING ALL STUDENTS, PUBLISHER NAME, 
 */
 
 function hasStudentReadAllBooksFromPublisher(books, students, publisherName, studentName){
+  //get a filtered list of only books from the given publisherName
+  const filteredBooks = getBooksByPublisher(books, publisherName)
+  const finishedBookIds = students[studentName].finished //[1,2]
 
+  let result = true;
+
+  filteredBooks.forEach((bookObj)=>{
+    //check if the current bookObj's id is not inside finishedBookIds array, set result = false
+    if(finishedBookIds.includes(bookObj.id) === false){
+      result = false
+    }
+  })
+  return result
 }
 
 
-// console.log(hasStudentReadAllBooksFromPublisher(books, students, "Simon & Schuster", "Spongebob"))
+function hasStudentReadAllBooksFromPublisherAdvanced(books, students, publisherName, studentName){
+  //get a filtered list of only books from the given publisherName
+  const filteredBooks = getBooksByPublisher(books, publisherName)
+  const finishedBookIds = students[studentName].finished //[1,2]
+
+  //check if every id in filteredBooks is inside of the finishedBookIds
+  const result = filteredBooks.every((bookObj)=>{
+    return finishedBookIds.includes(bookObj.id)
+  })
+  return result;
+}
+
+
+console.log(hasStudentReadAllBooksFromPublisher(books, students, "Simon & Schuster", "Spongebob"))
 
 /* 
 4. GIVEN AN OBJECT CONTAINING ALL STUDENTS, AND TWO STUDENT NAMES, DETERMINE IF THE FIRST STUDENT HAS READ ANY BOOKS THAT THE SECOND STUDENT HAS NOT READ YET. IF SO, RETURN TRUE. IF THE FIRST STUDENT HAS NOT READ ANY BOOKS THE SECOND STUDENT HAS NOT READ, RETURN FALSE.
